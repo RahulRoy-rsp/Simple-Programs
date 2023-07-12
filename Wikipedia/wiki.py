3a6014b6de3f8cfd6beedcd715b7dddce110dea8
Rahul Roy <91940155+RahulRoy-rsp@users.noreply.github.com>
2023-07-12 18:49:14 +0530

    program file

--- Wikipedia/wiki.py
+++ Wikipedia/wiki.py
@@ -2,3 +2,8 @@
 # pip install wikipedia
 
 import wikipedia as wp
+
+inp = input("Enter the text you want to search on Wikipedia: ")
+
+print(f" The title info for {inp} is: \n", wp.page(inp))
+print(f" The Summary of {inp} is: \n", wp.summary(inp))
