befa187a428ce9292764431bcd965a33ede9a1d3
Rahul Roy <91940155+RahulRoy-rsp@users.noreply.github.com>
2023-07-16 18:24:50 +0530

    Distinct Numbers in an array

--- Distinct/distinctNumbers.py
+++ Distinct/distinctNumbers.py
@@ -1,9 +1,8 @@
 from collections import Counter
 
-# arr = [87, 44, 21, 44, 2, 0, 45, 0, 7]
 arr = list(map(int, input("Enter numbers separted by space: ").split()))
-
 arrCount = Counter(arr)
+
 print("Distinct elements in the array: ")
 for i, j in arrCount.items():
   if j == 1:
