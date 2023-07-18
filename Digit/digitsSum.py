9d6771b78fefa41e35ac897ea8cb9ab6bd730ccd
Rahul Roy <91940155+RahulRoy-rsp@users.noreply.github.com>
2023-07-18 15:40:01 +0530

    sum of the digits of a number

--- Digit/digitsSum.py
+++ Digit/digitsSum.py
@@ -1,1 +1,10 @@
-print("HI"(
+num = int(input("Enter a number: "))
+
+sum_ = 0
+
+while num > 0:
+    curr = num % 10
+    sum_ += curr
+    num = num//10
+    
+print(f"Sum of the digits of the number is: {sum_}")
