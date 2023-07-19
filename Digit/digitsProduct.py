6347d26d4d2248ee9d040ccd330ad04b40830051
Rahul Roy <91940155+RahulRoy-rsp@users.noreply.github.com>
2023-07-19 12:40:13 +0530

    product of the digits of a positive number

--- Digit/digitsProduct.py
+++ Digit/digitsProduct.py
@@ -1,1 +1,14 @@
-digitsProduct
+num = int(input("Enter a number: "))
+
+prod = 1
+if num == 0:
+    print(f"Product of the digits of the number {num} is: 0")
+elif num > 0:
+    while num > 0:
+        curr = num % 10
+        prod *= curr
+        num = num//10
+        
+    print(f"Product of the digits of the number is: {prod}")
+else:
+    print("Please Enter a positive number")
